0e7b5234b1fde5a37b2f5cbed70fc918d35d2a7f
yuki4080 <yuki4080@gmail.com>
2025-03-31 14:22:56 +0900

    Update project

--- Assets/Scripts/OpenXR/OpenXRStartup.cs
+++ Assets/Scripts/OpenXR/OpenXRStartup.cs
@@ -1,13 +1,52 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Management;
 
 public class OpenXRStartup : MonoBehaviour
 {
+    public InputAction toggleVR;
+
+    private const float _defaultFieldOfView = 60.0f;
+    private Camera _mainCamera;
+    private bool _toggle = false;
+
+    private void OnEnable() => toggleVR.Enable();
+    private void OnDisable() => toggleVR.Disable();
+
     void Update()
     {
-        //if (XRController)
+        if (toggleVR.WasPerformedThisFrame())
         {
+            _toggle = !_toggle;
+
+            if (_toggle)
+            {
+                Debug.Log("Initializing XR...");
+                XRGeneralSettings.Instance.Manager.InitializeLoader();
+                if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+                {
+                    Debug.LogError("Initializing XR Failed.");
+                }
+                else
+                {
+                    Debug.Log("Starting XR...");
+                    XRGeneralSettings.Instance.Manager.StartSubsystems();
+                    Debug.Log("XR started.");
+                }
+            }
+            else
+            {
+                Debug.Log("Stopping XR...");
+                XRGeneralSettings.Instance.Manager.StopSubsystems();
+                Debug.Log("XR stopped.");
+
+                Debug.Log("Deinitializing XR...");
+                XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+                Debug.Log("XR deinitialized.");
+
+                _mainCamera.ResetAspect();
+                _mainCamera.fieldOfView = _defaultFieldOfView;
+            }
         }
     }
 }
--- Assets/Scripts/SetupTestScene.cs
+++ Assets/Scripts/SetupTestScene.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class SetupTestScene : MonoBehaviour
 {
     [SerializeField] private GameObject CardboardStartup;
-    
+    [SerializeField] private GameObject OpenXRStartup;
+
     void Awake()
     {
-        #if UNITY_CARDBOARD
+#if UNITY_CARDBOARD
         Instantiate(CardboardStartup, Vector3.zero, Quaternion.identity, this.transform);
-        #endif
+#elif UNITY_OPENXR
+        Instantiate(OpenXRStartup, Vector3.zero, Quaternion.identity, this.transform);
+#endif
     }
 }
